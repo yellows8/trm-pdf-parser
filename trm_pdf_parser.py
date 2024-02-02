a486938d026c96877469e13ba23ae9417b868e5d
yellows8 <yellows8@users.noreply.github.com>
2024-02-02 10:58:40 -0500

    Use 0b for the reset value instead of 0x, better logging, etc. Skip zero-reset-value by default.

--- trm_pdf_parser.py
+++ trm_pdf_parser.py
@@ -15,28 +15,36 @@ reg_text = None
 reg_offset = None
 reg_reset = None
 
+skip_zero_reset = True
+
 for pagei in range(page_end+1-page_start):
     page = reader.pages[page_start+pagei]
     page_text = page.extract_text()
     page_lines = page_text.split("\n")
+    found = False
 
     for i in range(len(page_lines)):
         line = page_lines[i]
-        tmp = line.find(reg_prefix)
-        if tmp!=-1:
-            reg_text = line[tmp:]
-            print(reg_text)
-            #print(page_lines[i+1])
-            #line = page_lines[i+1]
-            reg_offset = None
-            reg_reset = None
+        if reg_text is None:
+            tmp = line.find(reg_prefix)
+            if tmp!=-1:
+                reg_text = line[tmp:]
+                print(reg_text)
+                #print(page_lines[i+1])
+                #line = page_lines[i+1]
+                reg_offset = None
+                reg_reset = None
 
         tmp = -1
         if reg_text is not None:
             tmp = line.find("Offset")
+            #if tmp==-1:
+            #    print("Offset not found: %s" % (line))
         if tmp!=-1:
             line = line[tmp:]
             tmp = line.find("0x")
+            if tmp==-1:
+                print("Failed to find first '0x'.")
             if tmp!=-1:
                 tmpline = line[tmp:]
                 tmp = tmpline.find(" |")
@@ -44,27 +52,43 @@ for pagei in range(page_end+1-page_start):
                     tmp = tmpline.find(" │")
                 if tmp==-1:
                     tmp = tmpline.find(" I")
+                if tmp==-1:
+                    print("Failed to find delimator.")
                 if tmp!=-1:
                     reg_offset = tmpline[:tmp]
                     tmpline = tmpline[tmp:]
 
-                    tmp = tmpline.find("0x")
+                    tmp = tmpline.find('(0b')
+                    if tmp==-1:
+                        tmp = tmpline.find('0b')
+                    else:
+                        tmp = tmp+1
+                    if tmp==-1:
+                        print("Failed to find '0b'.")
+                        print(page_lines)
                     if tmp!=-1:
                         tmpline = tmpline[tmp:]
-                        tmp = tmpline.find(" (")
+                        tmp = tmpline.find(")")
                         if tmp!=-1:
                             reg_reset = tmpline[:tmp]
+                        else:
+                            reg_reset = tmpline
 
-                            reg_text = reg_text.replace(" ", "")
-                            reg_offset = reg_offset.replace(" ", "")
-                            reg_reset = reg_reset.replace(" ", "")
-                            reg_reset = reg_reset.replace("X", "0")
+                        reg_text = reg_text.replace(" ", "")
+                        reg_offset = reg_offset.replace(" ", "")
+                        reg_reset = reg_reset.replace(" ", "")
+                        reg_reset = reg_reset.replace("x", "0")
 
-                            print(reg_offset)
-                            print(reg_reset)
+                        if len(reg_reset)>34:
+                            reg_reset = reg_reset[:33]
 
+                        print(reg_offset)
+                        print(reg_reset)
+
+                        reg_reset = int(reg_reset, 2)
+                        if reg_reset!=0 or (skip_zero_reset is False and reg_reset==0):
                             reg_offset = "0x%X" % (int(reg_offset, 16) + offset_adjust)
-                            reg_reset = "0x%08X" % (int(reg_reset, 16))
+                            reg_reset = "0x%08X" % (reg_reset)
 
                             out_text = format_str.replace("{NAME}", reg_text)
                             out_text = out_text.replace("{OFFSET}", reg_offset)
@@ -72,10 +96,16 @@ for pagei in range(page_end+1-page_start):
                             out_text = out_text.replace("\\n", "\n")
                             outf.write(out_text)
 
-    if reg_text is None or reg_offset is None or reg_reset is None:
+                        found = True
+                        reg_text = None
+                        reg_offset = None
+                        reg_reset = None
+
+            if reg_offset is None or reg_reset is None:
+                reg_text = None
+                reg_offset = None
+                reg_reset = None
+
+    if found is False:
         print(page_lines)
-    else:
-        reg_text = None
-        reg_offset = None
-        reg_reset = None
 
